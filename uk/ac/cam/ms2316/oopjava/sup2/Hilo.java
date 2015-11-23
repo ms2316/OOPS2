cbbede1e257ba220b37e731031442ee07d03bf21
Mihails Smolins <ms2316@cam.ac.uk>
2015-11-23 00:57:13 +0000

    initial commit

---
+++ uk/ac/cam/ms2316/oopjava/sup2/Hilo.java
@@ -0,0 +1,17 @@
+package uk.ac.cam.ms2316.oopjava.sup2;
+
+public class Hilo {
+	
+	private int gamesPlayed;	
+
+	public Hilo() {
+		gamesPlayed = 0;
+	}
+
+	public void playNext() {
+		System.out.println("Enter your name then press enter");
+		
+
+	}
+
+}
